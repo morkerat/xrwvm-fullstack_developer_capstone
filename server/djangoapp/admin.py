1ecf5e2659b2dca466783cf3248302dd003b4246
morkerat <morkerat@example.com>
2026-09-01 14:55:35 -0400

    Add CarMake/CarModel, get_cars endpoint, sentiment analyzer integration

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
 # from .models import related models
 
 
 # Register your models here.
+from .models import CarMake, CarModel
+
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
